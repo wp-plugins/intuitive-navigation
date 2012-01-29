cf3292352ca701db72e20bfe1649dca7b8475d2b
Denis Buka <Denis Buka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-29 08:49:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/intuitive-navigation/trunk@496808 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- load-frame.php
+++ load-frame.php
@@ -5,7 +5,7 @@ $p = initIntNav ();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" >
-<head>  <meta name=”robots” content=”noindex”>
+<head>  <meta name="robots" content="noindex,nofollow" />
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>Intuitive Navigation Frame Content</title>
   <?php if ( ( trim( $options['int_nav_style_url'] ) == "" )  &&  ( $options['int_nav_style'] == 1 ) ) { ?>
